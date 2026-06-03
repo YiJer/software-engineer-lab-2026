64a1adc4c3a1a580cafc92874e0eadacf86fb845
YiJer <yijer.studio@gmail.com>
2026-06-03 23:13:40 +0800

    header_footer

---
+++ index.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navList = document.querySelector('.nav-list');
+
+
+// 點擊漢堡按鈕，切換漢堡選單的 open 類別
+menuBtn.addEventListener('click', function () {
+    navList.classList.toggle('active');
+    document.body.classList.toggle('menu-open');
+});
